fda4600f72b4ab5db6ee6332aaf66c8f835ed044
Giovanni Sabato <giovanni.sabato.94@gmail.com>
2020-03-17 19:22:23 -0400

    Update the simulator to use the Kaggle API

--- simulator.py
+++ simulator.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from kaggle.api.kaggle_api_extended import KaggleApi
 import csv
+import os
 
 class DailyData():
 	def __init__(self, date, start, high, low, close, volume, open_int):
@@ -15,6 +17,21 @@ class DailyData():
 # Import the data into the program
 #
 
+# Initialize the API
+api = KaggleApi()
+api.authenticate()
+
+# LOWERCASE. It matters.
+stock_ticker = "aapl"
+path = "Stocks/"+stock_ticker+".us.txt"
+data_stream = []
+
+# Create folder to store data in
+try:
+	os.mkdir("downloads")
+except OSError:
+	print("Creation of the directory 'downloads' failed: It might exist already")
+
 # Data format 
 # 0: Date
 # 1: Open
@@ -24,11 +41,11 @@ class DailyData():
 # 5: Volume
 # 6: OpenInt
 
-stock_ticker = "aapl.us.txt"
-data_file = "/home/g/gt/6242/6242_proj/Stocks/" + stock_ticker
-data_stream = []
+# Get the data from the API
+api.dataset_download_file("borismarjanovic/price-volume-data-for-all-us-stocks-etfs", path, path="downloads")
+print("Downloaded: %s.us.txt" % (stock_ticker))
 
-with open(data_file, newline='') as csvfile:
+with open(path, newline='') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',')
 	header = next(reader, None)
 	for row in reader:
@@ -45,5 +62,5 @@ with open(data_file, newline='') as csvfile:
 #
 
 for day in data_stream:
-	print("Date: %s Open: %f High: %f Low: %f Close: %f Volume: %d OpenInt: %d" %
+	print("Date: %s    Open: %f    High: %f    Low: %f    Close: %f    Volume: %d    OpenInt: %d" %
 	(day.date.strftime("%Y-%m-%d"), day.start, day.high, day.low, day.close, day.volume, day.open_int))
