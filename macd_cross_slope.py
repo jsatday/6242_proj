833075674f632be2fb040bc889fe508cd5ad0aef
Giovanni Sabato <giovanni.sabato.94@gmail.com>
2020-04-09 00:06:28 -0400

    macd_cross_slope now uses new ctx. Playing with optimization.

--- macd_cross_slope.py
+++ macd_cross_slope.py
@@ -1,5 +1,6 @@
 from scipy import signal
-from trade_points import trade_points
+from trade_context import trade_context
+import trade_context as tc
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,47 +8,62 @@ import matplotlib as mpl
 
 
 def macd_cross_slope(df, start=0, end=-1):
-	in_trade = False
-	window_size = 9
-	macd = []
+	window_size = 15
 	macd_smooth = []
-	itrades = trade_points([],[],[],[])
-	ptrades = trade_points([],[],[],[])
+	t_ctx = trade_context()
 
-	for i in range(start,end):
-		macd.append(df.trend_macd[i])
+	for i in range(start, end):
 
 		# Wait for enough data to start smoothing the curve
-		if len(macd) > window_size:
-			macd_smooth = signal.savgol_filter(macd, window_size, 1)
+		if i-start >= window_size:
+			macd_smooth = signal.savgol_filter(df.trend_macd[start:i+1], window_size, 1)
+		else:
+			continue
 
-			# Buy the stock
-			if (macd_smooth[-1]-macd_smooth[-2] > 0 and
-				# macd_smooth[-1] < df.trend_macd_signal[i] and
-				not in_trade):
+		# if i-start >= window_size:
+		# 	macd_smooth.append(signal.savgol_filter(df.trend_macd[i-window_size+1:i+1], window_size, 1)[-1])
+		# else:
+		# 	macd_smooth.append(df.trend_macd[i])
+		# 	continue
 
-				in_trade = True
-				itrades.buy_xs.append(i)
-				itrades.buy_ys.append(macd_smooth[-1])
-				ptrades.buy_xs.append(i)
-				ptrades.buy_ys.append(df.Close[i])
-				continue
+		#
+		# Advanced rules
+		#
 
-			# Sell the stock
-			if (macd_smooth[-1]-macd_smooth[-2] < 0 and
-				# macd_smooth[-1] > df.trend_macd_signal[i] and
-				in_trade):
+		# Don't trade anything under 10
+		if df.Close[i] < 10 and not t_ctx.in_trade:
+			continue
 
-				in_trade = False
-				itrades.sell_xs.append(i)
-				itrades.sell_ys.append(macd_smooth[-1])
-				ptrades.sell_xs.append(i)
-				ptrades.sell_ys.append(df.Close[i])
+		# if t_ctx.in_trade:
+		# 	t_ctx.price_diff_cur = df.Close[i] - t_ctx.buy_price[-1]
+		# 	price_diff_high = t_ctx.price_diff_cur if t_ctx.price_diff_cur > price_diff_high else price_diff_high
+		# 	if t_ctx.price_diff_cur < price_diff_high/2:
+
+
+
+		#
+		# Typical entry and exit
+		#
+
+		# Buy the stock
+		if (macd_smooth[-1]-macd_smooth[-2] > 0 and
+			macd_smooth[-1] < df.trend_macd_signal[i] and
+			not t_ctx.in_trade):
+
+			tc.buy_stock(t_ctx, i, df.Close[i], macd_smooth[-1])
+			continue
+
+		# Sell the stock
+		if (macd_smooth[-1]-macd_smooth[-2] < 0 and
+			# macd_smooth[-1] > df.trend_macd_signal[i] and
+			t_ctx.in_trade):
+
+			tc.sell_stock(t_ctx, i, df.Close[i], macd_smooth[-1])
 				
-	return ptrades, itrades, macd_smooth
+	return t_ctx, macd_smooth
 
 
-def plot_macd_cross_slope(df, ptrades, itrades, macd_smooth, start=0, end=-1):
+def plot_macd_cross_slope(df, t_ctx, macd_smooth, start=0, end=-1):
 	mpl.style.use('seaborn')
 
 	fig, axs = plt.subplots(2, sharex=True)
@@ -59,8 +75,8 @@ def plot_macd_cross_slope(df, ptrades, itrades, macd_smooth, start=0, end=-1):
 	axs[0].plot(df[start:end].Close, label='Stock Price')
 
 	# Plot the buys/sells on the price
-	axs[0].plot(ptrades.buy_xs, ptrades.buy_ys, 'ro', color='green', ms=5)
-	axs[0].plot(ptrades.sell_xs, ptrades.sell_ys, 'ro', color='red', ms=5)
+	axs[0].plot(t_ctx.buy_times, t_ctx.buy_prices, 'ro', color='green', ms=5)
+	axs[0].plot(t_ctx.sell_times, t_ctx.sell_prices, 'ro', color='red', ms=5)
 
 	# Plot the MACDs
 	axs[1].plot(df[start:end].trend_macd, label='MACD')
@@ -68,8 +84,8 @@ def plot_macd_cross_slope(df, ptrades, itrades, macd_smooth, start=0, end=-1):
 	axs[1].plot([i for i in range(start,end)], macd_smooth, label='MACD Smooth', color='black')
 	
 	# Plot the buys/sells on the MACD
-	axs[1].plot(itrades.buy_xs, itrades.buy_ys, 'ro', color='green', ms=5)
-	axs[1].plot(itrades.sell_xs, itrades.sell_ys, 'ro', color='red', ms=5)
+	axs[1].plot(t_ctx.buy_times, t_ctx.buy_inds, 'ro', color='green', ms=5)
+	axs[1].plot(t_ctx.sell_times, t_ctx.sell_inds, 'ro', color='red', ms=5)
 
 
 def trade_with_macd_cross_slope(df, start=0, end=-1, plot=False):
@@ -77,11 +93,10 @@ def trade_with_macd_cross_slope(df, start=0, end=-1, plot=False):
 	end = len(df.Close) if end==-1 else end
 
 	# Simulate the Trades
-	ptrades, itrades, macd_smooth = macd_cross_slope(df, start, end)
+	t_ctx, macd_smooth = macd_cross_slope(df, start, end)
 
 	# Plot the data
 	if plot:
-		plot_macd_cross_slope(df, ptrades, itrades, macd_smooth, start, end)
-		plt.show()
+		plot_macd_cross_slope(df, t_ctx, macd_smooth, start, end)
 
-	return ptrades
+	return t_ctx
